f186b9740d8a4ed07f27bb08bf194a0982a04986
Eirik Aflekt <eirik.thomessen@bouvet.no>
2021-03-05 10:25:54 +0100

    Initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -4,25 +4,179 @@ void main() {
   runApp(MyApp());
 }
 
+const PrimaryColor = Color(0xFF006964);
+const MaterialColor kPrimaryColor = const MaterialColor(
+  0xFF006964,
+  const <int, Color>{
+    50: const Color(0xFF006964),
+    100: const Color(0xFF006964),
+    200: const Color(0xFF006964),
+    300: const Color(0xFF006964),
+    400: const Color(0xFF006964),
+    500: const Color(0xFF006964),
+    600: const Color(0xFF006964),
+    700: const Color(0xFF006964),
+    800: const Color(0xFF006964),
+    900: const Color(0xFF006964),
+  },
+);
+
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    var tv = TabBarView(
+      children: [
+        MyHomePage(title: 'Registrer'),
+        HistoryPage(title: 'Historikk'),
+        RegisterPage(),
+      ],
+    );
+
+    var def = DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        bottomNavigationBar: menu(),
+        body: tv,
+      ),
+    );
+
+    var themeData = ThemeData(
+      // This is the theme of your application.
+      //
+      // Try running your application with "flutter run". You'll see the
+      // application has a blue toolbar. Then, without quitting the app, try
+      // changing the primarySwatch below to Colors.green and then invoke
+      // "hot reload" (press "r" in the console where you ran "flutter run",
+      // or simply save your changes to "hot reload" in a Flutter IDE).
+      // Notice that the counter didn't reset back to zero; the application
+      // is not restarted.
+      primarySwatch: kPrimaryColor,
+      // This makes the visual density adapt to the platform that you run
+      // the app on. For desktop platforms, the controls will be smaller and
+      // closer together (more dense) than on mobile platforms.
+      visualDensity: VisualDensity.adaptivePlatformDensity,
+    );
+
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+      theme: themeData,
+
+      // home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: def,
+    );
+  }
+}
+
+Widget menu() {
+  return Container(
+    color: PrimaryColor,
+    child: TabBar(
+      labelColor: Colors.white,
+      unselectedLabelColor: Colors.white70,
+      indicatorSize: TabBarIndicatorSize.tab,
+      indicatorPadding: EdgeInsets.all(5.0),
+      indicatorColor: PrimaryColor,
+      tabs: [
+        Tab(
+          text: "Registrer",
+          icon: Icon(Icons.send),
+        ),
+        Tab(
+          text: "Historikk",
+          icon: Icon(Icons.insert_chart),
+        ),
+        Tab(
+          text: "Meg",
+          icon: Icon(Icons.account_circle),
+        ),
+      ],
+    ),
+  );
+}
+
+class HistoryPage extends StatefulWidget {
+  HistoryPage({Key key, this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  _HistoryPageState createState() => _HistoryPageState();
+}
+
+class _HistoryPageState extends State<HistoryPage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+        centerTitle: true,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+          // Column is also a layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          //
+          // Invoke "debug painting" (press "p" in the console, choose the
+          // "Toggle Debug Paint" action from the Flutter Inspector in Android
+          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+          // to see the wireframe for each widget.
+          //
+          // Column has various properties to control how it sizes itself and
+          // how it positions its children. Here we use mainAxisAlignment to
+          // center the children vertically; the main axis here is the vertical
+          // axis because Columns are vertical (the cross axis would be
+          // horizontal).
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
@@ -71,43 +225,135 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text(widget?.title),
+        centerTitle: true,
       ),
       body: Center(
+        child: RegisterPage(),
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
+        // child: Column(
+        //   // Column is also a layout widget. It takes a list of children and
+        //   // arranges them vertically. By default, it sizes itself to fit its
+        //   // children horizontally, and tries to be as tall as its parent.
+        //   //
+        //   // Invoke "debug painting" (press "p" in the console, choose the
+        //   // "Toggle Debug Paint" action from the Flutter Inspector in Android
+        //   // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+        //   // to see the wireframe for each widget.
+        //   //
+        //   // Column has various properties to control how it sizes itself and
+        //   // how it positions its children. Here we use mainAxisAlignment to
+        //   // center the children vertically; the main axis here is the vertical
+        //   // axis because Columns are vertical (the cross axis would be
+        //   // horizontal).
+        //   mainAxisAlignment: MainAxisAlignment.center,
+        //   children: <Widget>[
+        //     Text(
+        //       'You have pushed the button this many times:',
+        //     ),
+        //     Text(
+        //       '$_counter',
+        //       style: Theme.of(context).textTheme.headline4,
+        //     ),
+        //   ],
+        // ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+// Define a custom Form widget.
+class RegisterPage extends StatefulWidget {
+  @override
+  RegisterPageState createState() => RegisterPageState();
+}
+
+// Define a corresponding State class.
+// This class holds data related to the form.
+class RegisterPageState extends State<RegisterPage> {
+  // Create a global key that uniquely identifies the Form widget
+  // and allows validation of the form.
+  //
+  // Note: This is a GlobalKey<FormState>,
+  // not a GlobalKey<MyCustomFormState>.
+  final _formKey = GlobalKey<FormState>();
+  var testIcon = Icon(Icons.done);
+  var testIcon2 = Icon(Icons.send);
+
+  bool _isValid = true;
+
+  void _toggleFavorite() {
+    setState(() {
+      if (_isValid) {
+        _isValid = false;
+      } else {
+        _isValid = true;
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // Build a Form widget using the _formKey created above.
+    return Form(
+      key: _formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              IconButton(
+                padding: EdgeInsets.all(0),
+                alignment: Alignment.centerLeft,
+                icon: Icon(Icons.error),
+                color: PrimaryColor,
+                onPressed: _toggleFavorite,
+              ),
+              Flexible(
+                child: Text(
+                    "Send inn en registrering for hver jaktdag, for hele jaktfølget tet",
+                    ),
+              ),
+            ],
+          ),
+          TextFormField(
+            decoration: InputDecoration(
+                labelText: 'Jegernummer',
+                suffixIcon: (_isValid ? Icon(Icons.done) : Icon(Icons.error))),
+            validator: (value) {
+              if (value.isEmpty) {
+                // _toggleFavorite();
+                return 'Please enter some text';
+              }
+              return null;
+            },
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16.0),
+            child: ElevatedButton(
+              onPressed: () {
+                // Validate returns true if the form is valid, or false
+                // otherwise.
+                _toggleFavorite();
+
+                if (_formKey.currentState.validate()) {
+                  // If the form is valid, display a Snackbar.
+                  // ScaffoldMessenger.of(context)
+                  //     .showSnackBar(SnackBar(content: Text('Processing Data')));
+                }
+              },
+              child: Text('Submit'),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
