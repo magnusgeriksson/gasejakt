76d98eece73fae65413ac4467f2ca6b2d5c14c90
Eirik Aflekt <eirik.thomessen@bouvet.no>
2021-03-08 16:44:15 +0100

    Validate jegernummer

--- lib/ui/screens/register_screen.dart
+++ lib/ui/screens/register_screen.dart
@@ -14,95 +14,84 @@ class _RegisterState extends State<RegisterScreen> {
   var testIcon = Icon(Icons.done);
   var testIcon2 = Icon(Icons.send);
 
-  bool _isValid = true;
-
-  void _toggleFavorite() {
-    setState(() {
-      if (_isValid) {
-        _isValid = false;
-      } else {
-        _isValid = true;
-      }
-    });
-  }
-
   @override
   void initState() {
     //model.loadData(); <-- Hent huntingdaymodel med jegernummer registrert?
     super.initState();
   }
 
+  // bool _isValid = true;
+
+  void _toggleValidator(bool valid) {
+    setState(() {
+      // _isValid = valid;
+    });
+  }
+  final myController = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider<RegisterViewModel>.value(
-      value: viewModel,
-      child: Consumer<RegisterViewModel>(
-        builder: (context, viewModel, child) => Scaffold(
-          appBar: AppBar(
-            title: Text(viewModel.testString),
-          ),
-          body: Column(
-            children: [
-              Form(
-                key: _formKey,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: <Widget>[
-                        IconButton(
-                          padding: EdgeInsets.all(0),
-                          alignment: Alignment.centerLeft,
-                          icon: Icon(Icons.error),
-                          color: Colors.green[700],
-                          onPressed: _toggleFavorite,
-                        ),
-                        Flexible(
-                          child: Text(
-                            "Send inn en registrering for hver jaktdag, for hele jaktfølget tet",
-                          ),
-                        ),
-                      ],
+    // Build a Form widget using the _formKey created above.
+    return Container(
+        padding: EdgeInsets.all(25),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+                  IconButton(
+                    padding: EdgeInsets.all(0),
+                    alignment: Alignment.centerLeft,
+                    icon: Icon(Icons.error),
+                    //TODO use constant
+                    color: Color(0xFF006964),
+                  ),
+                  Flexible(
+                    child: Text(
+                      "Send inn en registrering for hver jaktdag, for hele jaktfølget tet",
                     ),
-                    TextFormField(
-                      decoration: InputDecoration(
-                          labelText: 'Jegernummer',
-                          suffixIcon: (_isValid
-                              ? Icon(Icons.done)
-                              : Icon(Icons.error))),
-                      validator: (value) {
-                        if (value.isEmpty) {
-                          // _toggleFavorite();
-                          return 'Please enter some text';
-                        }
-                        return null;
-                      },
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(vertical: 16.0),
-                      child: ElevatedButton(
-                        onPressed: () {
-                          // Validate returns true if the form is valid, or false
-                          // otherwise.
-                          _toggleFavorite();
+                  ),
+                ],
+              ),
+              TextFormField(
+                decoration: InputDecoration(
+                    labelText: 'Jegernummer',
+                    suffixIcon: ((_formKey?.currentState?.validate() ?? false)
+                        ? Icon(Icons.done)
+                        : Icon(Icons.error))),
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return 'Please enter some text';
+                  }
+                  return null;
+                },
+                controller: myController,
+                onChanged: (text){
+                  _toggleValidator(_formKey?.currentState?.validate());
+                  // _formKey.currentState.validate();
+                } ,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 16.0),
+                child: ElevatedButton(
+                  onPressed: () {
+                    // Validate returns true if the form is valid, or false
+                    // otherwise.
+                    _toggleValidator(_formKey.currentState.validate());
 
-                          if (_formKey.currentState.validate()) {
-                            // If the form is valid, display a Snackbar.
-                            // ScaffoldMessenger.of(context)
-                            //     .showSnackBar(SnackBar(content: Text('Processing Data')));
-                          }
-                        },
-                        child: Text('Submit'),
-                      ),
-                    ),
-                  ],
+                    if (_formKey.currentState.validate()) {
+                      // If the form is valid, display a Snackbar.
+                      // ScaffoldMessenger.of(context)
+                      //     .showSnackBar(SnackBar(content: Text('Processing Data')));
+                    }
+                  },
+                  child: Text('Send inn'),
                 ),
-              )
+              ),
             ],
           ),
-        ),
-      ),
-    );
+        ));
   }
 }
